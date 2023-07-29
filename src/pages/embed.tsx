4d0c05fe9c307a64626a58e2068de84dff33fe33
Nguyen Phi Long <philongit1999@gmail.com>
2023-07-29 15:14:30 +0700

    update

--- src/pages/embed.tsx
+++ src/pages/embed.tsx
@@ -7,15 +7,11 @@ const EmbeddedWebsitePage = () => {
     <div>
       <h1>Embedded Website Page</h1>
       <iframe
-        src="https://dchart.vndirect.com.vn/?symbol=ACB&theme=dark" // Replace this URL with the website you want to embed
+        src="https://dstock.vndirect.com.vn/tong-quan/KHP/stock-chart-popup" // Replace this URL with the website you want to embed
         width="100%"
         height="800"
+        scrolling="auto"
       />
-      sdfdsfdsfd
-      <iframe
-        // style="border:0;width:100%;height:70vh"
-        src="https://dchart.vndirect.com.vn/?symbol=KHP&theme=dark"
-      ></iframe>
     </div>
   );
 };
